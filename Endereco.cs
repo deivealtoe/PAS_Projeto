bda637517665e45f23a3dbc5c1c9181e14227a96
Deivisson Altoé <deive@ucl.br>
2020-11-13 23:13:36 -0300

    Criado os métodos da classe abstrata Pessoa

--- Endereco.cs
+++ Endereco.cs
@@ -2,10 +2,15 @@ namespace Projeto2
 {
     class Endereco
     {
+
         private string endereco;
         private string bairro;
         private string cidade;
         private string estado;
         private string pais;
+
+
+        
+
     }
 }
--- Pessoa.cs
+++ Pessoa.cs
@@ -1,11 +1,89 @@
 namespace Projeto2
 {
-    class Pessoa
+    abstract class Pessoa
     {
+
+        private int codigo;
         private string nome;
-        private string sobreNome;
+        private string sobrenome;
         private string razaoSocial;
         private string cpfCnpj;
-        private Endereco enderecoCompleto;
+        private Endereco endereco;
+
+
+
+        public Pessoa(int codigo, string nome, string sobrenome, string cpfCnpj, Endereco endereco) {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.cpfCnpj = cpfCnpj;
+            this.endereco = endereco;
+        }
+
+
+        public Pessoa (int codigo, string razaoSocial, string cpfCnpj, Endereco endereco) {
+            this.codigo = codigo;
+            this.razaoSocial = razaoSocial;
+            this.cpfCnpj = cpfCnpj;
+            this.endereco = endereco;
+        }
+
+
+
+        public int getCodigo() {
+            return this.codigo;
+        }
+
+
+        public string getNome() {
+            return this.nome;
+        }
+
+
+        public string getSobrenome() {
+            return this.sobrenome;
+        }
+
+
+        public string getRazaoSocial() {
+            return this.razaoSocial;
+        }
+
+
+        public string getCpfCnpj() {
+            return this.cpfCnpj;
+        }
+
+
+        public Endereco getEndereco() {
+            return this.endereco;
+        }
+
+
+
+        public void setNome(string nome) {
+            this.nome = nome;
+        }
+
+
+        public void setSobrenome(string sobrenome) {
+            this.sobrenome = sobrenome;
+        }
+
+
+        public void setCpfCnpj(string cpfCnpj) {
+            this.cpfCnpj = cpfCnpj;
+        }
+
+
+        public void setRazaoSocial(string razaoSocial) {
+            this.razaoSocial = razaoSocial;
+        }
+
+
+        public void setEndereco(Endereco endereco) {
+            this.endereco = endereco;
+        }
+
     }
 }
